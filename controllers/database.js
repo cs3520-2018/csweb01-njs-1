fce3b26af8bffbdd5f06091599518ea4c2781aa4
cs3520-2018 <cs3520-2018>
2018-05-27 18:36:03 -0700

    Adding billingInfo

--- controllers/database.js
+++ controllers/database.js
@@ -209,7 +209,7 @@ module.exports.storeData = async function( data )
 			// getCustomerId() checks if there are no duplicate customers with same email. If checking for duplicates isn't important, then use insert() helper function instead and create document.
 			//let customerId = await getCustomerId( db, data.checkoutInfo.billingInfo );
  console.log('Printing checkoutInfo.billingInfo ' + checkoutInfo);
-			let customerId = await insert( db, "customers", data.checkoutInfo );
+			let customerId = await insert( db, "customers", data.checkoutInfo.billingInfo );
 			if ( customerId != null )
 			{
             console.log("if customerID != NULL");
